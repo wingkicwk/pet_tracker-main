ea087febdafa04a185ccaa47f2820a10e9abcc07
YiZhang <56272051+sdzy9412@users.noreply.github.com>
2020-11-15 13:43:06 +0000

    modify setting

--- pet_tracker/pet_tracker/settings.py
+++ pet_tracker/pet_tracker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'pet_tracker.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,'templates')],
+        'DIRS': [os.path.join(BASE_DIR,'pet_tracker/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -68,7 +68,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'pet_tracker.wsgi.application'
-
+print("here: ", BASE_DIR)
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
