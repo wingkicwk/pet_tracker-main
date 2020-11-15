f70c3b0b0b03db7027677c8256d437a491382d71
wingki <mnicwk@gmail.com>
2020-11-15 13:55:12 +0000

    added str

--- pet_tracker/pet_tracker/settings.py
+++ pet_tracker/pet_tracker/settings.py
@@ -76,7 +76,7 @@ print("here: ", BASE_DIR)
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
