e7227ddb3b83d0a7e02cbb2b3ee78f65d4fc2544
YiZhang <56272051+sdzy9412@users.noreply.github.com>
2020-11-17 02:19:19 +0000

    markers by click

--- pet_tracker/pet_tracker/static/javascript/map.js
+++ pet_tracker/pet_tracker/static/javascript/map.js
@@ -1,30 +1,53 @@
-let markers = [];
 let map;
+let markers = [];
 
 function initMap() {
-  x=navigator.geolocation;
-  x.getCurrentPosition(success);
+    //show map
+    var coord = new google.maps.LatLng({ lat: 53.350140, lng: -6.266155 });
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 53.350140, lng: -6.266155 },
+        zoom: 13,
+    });
 
-  function success(position){
-      var myLat = position.coords.latitude;
-      var myLong = position.coords.longitude;
+    //create a button
+    const locationButton = document.createElement("button");
+    locationButton.textContent = "myLocation";
+    locationButton.classList.add("custom-map-control-button");
+    map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
 
-      var coords = new google.maps.LatLng(myLat, myLong);
+    //get user location by click on the button
+    locationButton.addEventListener("click", () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+        (position) => {
+        const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+        };
+        map.setCenter(pos);
+        },
+        () => {
+            handleLocationError(true, infoWindow, map.getCenter());
+        }
+        );
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+    });
 
-      var mapOptions = {
-          zoom: 16,
-          center: coords,
-      }
-
-      map = new google.maps.Map(document.getElementById("map"), mapOptions);
-      var marker = new google.maps.Marker({
-          map:map,
-          position:coords,
-          draggable: true,
-          title: "Drag me!"
-      });
-  }
-
-
+    //place a marker
+    google.maps.event.addListener(map, 'click', function(event) {
+    var marker = new google.maps.Marker({
+        position: event.latLng,
+        draggable:true,
+        title: "Hello world!"
+    });
+    markers.push(marker);
+    marker.setMap(map);
+    console.log(markers);
+//    console.log(marker.position);
+    });
+//    console.log(markers);
 
 }
