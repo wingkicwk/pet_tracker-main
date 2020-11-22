c52802a2135d6d64f8824b25d193dbe430562ffb
wbw1124 <34853414+wbw1124@users.noreply.github.com>
2020-11-22 10:51:21 +0000

    Update settings.py
      #'django.middleware.csrf.CsrfViewMiddleware',此行需要注释掉，否则会403

--- pet_tracker/pet_tracker/settings.py
+++ pet_tracker/pet_tracker/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',此行需要注释掉，否则会403
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
