642f01b472fd55652810705aa4b066e523eb97ee
EC2 Default User <ec2-user@ip-172-31-36-15.eu-west-1.compute.internal>
2020-11-30 09:33:18 +0000

    deploy

--- pet_tracker/pet_tracker/settings.py
+++ pet_tracker/pet_tracker/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.250.42.253']
 
 
 INSTALLED_APPS = [
